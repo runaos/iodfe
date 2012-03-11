484556546b3a7a15fac6fcd9f2c801239439a65f
runaos <runaos@gmail.com>
2012-03-11 16:08:58 +0400

    timescale no longer affects sv_fps

--- code/server/sv_main.c
+++ code/server/sv_main.c
@@ -1077,11 +1077,10 @@ void SV_Frame( int msec ) {
 		Cvar_Set( "sv_fps", "10" );
 	}
 
-	frameMsec = 1000 / sv_fps->integer * com_timescale->value;
+	frameMsec = 1000 / sv_fps->integer ;
 	// don't let it scale below 1ms
 	if(frameMsec < 1)
 	{
-		Cvar_Set("timescale", va("%f", sv_fps->integer / 1000.0f));
 		frameMsec = 1;
 	}
 
